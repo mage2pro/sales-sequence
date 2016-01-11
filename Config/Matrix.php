62681d46b4c1c659bc6799a097b1d681d39b214c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-01-11 11:46:02 +0300

    no message

--- Config/Matrix.php
+++ Config/Matrix.php
@@ -10,10 +10,27 @@ class Matrix extends Hidden {
 	 * @return void
 	 */
 	public function onFormInitialized() {
-		$this['value'] = df_json_encode([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]);
+		$this['value'] = df_json_encode($this->nextNumbers());
 		df_fe_init($this, __CLASS__, 'Df_Core::lib/Handsontable/main.css', [
-			'columns' => ['Order', 'Invoice', 'Shipment', 'Credit Memo']
-			,'rows' => ['Store 1', 'Store 2', 'Store 3']
+			'columns' => array_keys(df_sales_entity_types())
+			// 2016-01-11
+			// array_values надо ставить обязательно,
+			// потому что когда опция Handsontable rowHeaders
+			// у ассоциативного массива берёт почему-то ключи, а не значения.
+			// http://docs.handsontable.com/0.20.2/Options.html#rowHeaders
+			,'rows' => array_values(df_store_names())
 		]);
+	}
+
+	/**
+	 * 2016-01-11
+	 * @return int[][]
+	 */
+	private function nextNumbers() {
+		return array_map(function($storeId) {
+			return array_values(array_map(function($entityTypeId) use ($storeId) {
+				return df_sales_seq_next($entityTypeId, $storeId);
+			}, df_sales_entity_types()));
+		}, df_store_ids());
 	}
 }
