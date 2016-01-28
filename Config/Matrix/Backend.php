7730d822d406dc7ef3b3530430ac7f5397c5d44f
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-01-28 03:23:16 +0300

    no message

--- Config/Matrix/Backend.php
+++ Config/Matrix/Backend.php
@@ -1,30 +1,73 @@
 <?php
 namespace Dfe\SalesSequence\Config\Matrix;
-use Df\Config\BackendUnusial;
+use Df\Config\Backend\Unusial\Model;
 use Df\Config\Backend as _Backend;
 /**
  * 2016-01-11
  * @method mixed|null getValue()
  */
-class Backend extends BackendUnusial {
+class Backend extends Model {
 	/**
 	 * 2016-01-14
 	 * @override
-	 * @see \Df\Config\BackendUnusial::afterLoad()
+	 * @see \Df\Config\Backend\Unusial\Model::afterLoad()
 	 * @return $this
 	 */
 	public function afterLoad() {
-		$this->setValue(df_json_encode($this->nextNumbers()));
+		$this->setValue(df_json_encode($this->nextNumbersFromDb()));
+		return $this;
+	}
+
+	/**
+	 * 2016-01-26
+	 * @override
+	 * @see \Df\Config\Backend\Unusial\Model::delete()
+	 * @return $this
+	 */
+	public function delete() {
 		return $this;
 	}
 
 	/**
 	 * 2016-01-11
+	 * 2016-01-27
+	 * Мы не можем выполнить наши задачи внутри метода save(),
+	 * потому что нам нужно изменить значение AUTO INCREMENT, а это недопустимо внутри транзакции
+	 * «Something went wrong while saving this configuration:
+	 * User Error: DDL statements are not allowed in transactions».
 	 * @override
-	 * @see \Df\Config\BackendUnusial::save()
+	 * @see \Magento\Framework\Model\AbstractModel::afterCommitCallback()
+	 * @used-by \Df\Config\Backend\Unusial\Model::save()
 	 * @return $this
 	 */
-	public function save() {
+	public function afterCommitCallback() {
+		/** @var int[][] $valuesFromUi */
+		$valuesFromUi = df_json_decode($this->getValue());
+		/** @var int[][] $valuesFromDb */
+		$valuesFromDb = $this->nextNumbersFromDb();
+		/** @var int[] $storeIds */
+		$storeIds = df_store_ids();
+		/** @var int $numStores */
+		$numStores = count($storeIds);
+		/** @var int[] $types */
+		// 2016-01-27
+		// Нам нужно, чтобы у массива были целочисленные ключи.
+		$types = array_values(df_sales_entity_types());
+		/** @var int $numTypes */
+		$numTypes = count($types);
+		for ($storeIndex = 0; $storeIndex < $numStores; $storeIndex++) {
+			/** @var int $storeIndex */
+			for ($typeIndex = 0; $typeIndex < $numTypes; $typeIndex++) {
+				/** @var int $typeIndex */
+				/** @var int $valueFromUi */
+				$valueFromUi = (int)$valuesFromUi[$storeIndex][$typeIndex];
+				/** @var int $valueFromDb */
+				$valueFromDb = (int)$valuesFromDb[$storeIndex][$typeIndex];
+				if ($valueFromUi !== $valueFromDb) {
+					$this->updateNextNumber($storeIds[$storeIndex], $types[$typeIndex], $valueFromUi);
+				}
+			}
+		}
 		return $this;
 	}
 
@@ -32,11 +75,24 @@ class Backend extends BackendUnusial {
 	 * 2016-01-11
 	 * @return int[][]
 	 */
-	private function nextNumbers() {
+	private function nextNumbersFromDb() {
 		return array_map(function($storeId) {
 			return array_values(array_map(function($entityTypeId) use ($storeId) {
 				return df_sales_seq_next($entityTypeId, $storeId);
 			}, df_sales_entity_types()));
 		}, df_store_ids());
+	}
+
+	/**
+	 * 2016-01-26
+	 * @param int $storeId
+	 * @param string $entityTypeCode
+	 * @param int $nextNumber
+	 * @return void
+	 */
+	private function updateNextNumber($storeId, $entityTypeCode, $nextNumber) {
+		/** @var string $table */
+		$table = df_sales_seq_meta($entityTypeCode, $storeId)->getSequenceTable();
+		df_next_increment_set($table, max(df_fetch_col_max($table, 'sequence_value'), $nextNumber));
 	}
 }
---
+++ Model/Meta.php
@@ -0,0 +1,7 @@
+<?php
+namespace Dfe\SalesSequence\Model;
+/**
+ * 2016-01-26
+ * @method string getSequenceTable()
+ */
+class Meta extends \Magento\SalesSequence\Model\Meta {}
