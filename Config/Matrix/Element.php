8d100e656f3a8e66a9bfbbcaa5844c305aeb6a9c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-01-29 22:21:14 +0300

    no message

---
+++ Config/Matrix/Element.php
@@ -0,0 +1,24 @@
+<?php
+namespace Dfe\SalesSequence\Config\Matrix;
+use Df\Framework\Data\Form\Element\Hidden;
+abstract class Element extends Hidden {
+	/**
+	 * 2016-01-29
+	 * @return string[]
+	 */
+	abstract protected function rows();
+
+	/**
+	 * 2016-01-29
+	 * @override
+	 * @see \Df\Framework\Data\Form\ElementI::onFormInitialized()
+	 * @used-by \Df\Framework\Plugin\Data\Form\Element\AbstractElement::afterSetForm()
+	 * @return void
+	 */
+	public function onFormInitialized() {
+		df_fe_init($this, __CLASS__, 'Df_Core::lib/Handsontable/main.css', [
+			'columns' => array_keys(df_sales_entity_types())
+			, 'rows' => $this->rows()
+		], 'matrix');
+	}
+}
--- Config/Next/Element.php
+++ Config/Next/Element.php
@@ -1,23 +1,18 @@
 <?php
 namespace Dfe\SalesSequence\Config\Next;
-use Df\Framework\Data\Form\Element\Hidden;
-class Element extends Hidden {
+use Dfe\SalesSequence\Config\Matrix\Element as _Element;
+class Element extends _Element {
 	/**
-	 * 2016-01-10
+	 * 2016-01-29
 	 * @override
-	 * @see \Df\Framework\Data\Form\ElementI::onFormInitialized()
-	 * @used-by \Df\Framework\Plugin\Data\Form\Element\AbstractElement::afterSetForm()
-	 * @return void
+	 * @see \Dfe\SalesSequence\Config\Matrix\Element::rows()
+	 * @used-by \Dfe\SalesSequence\Config\Matrix\Element::onFormInitialized()
+	 * 2016-01-11
+	 * array_values надо ставить обязательно,
+	 * потому что когда опция Handsontable rowHeaders
+	 * у ассоциативного массива берёт почему-то ключи, а не значения.
+	 * http://docs.handsontable.com/0.20.2/Options.html#rowHeaders
+	 * @return string[]
 	 */
-	public function onFormInitialized() {
-		df_fe_init($this, __CLASS__, 'Df_Core::lib/Handsontable/main.css', [
-			'columns' => array_keys(df_sales_entity_types())
-			// 2016-01-11
-			// array_values надо ставить обязательно,
-			// потому что когда опция Handsontable rowHeaders
-			// у ассоциативного массива берёт почему-то ключи, а не значения.
-			// http://docs.handsontable.com/0.20.2/Options.html#rowHeaders
-			,'rows' => array_values(df_store_names())
-		], 'matrix');
-	}
+	protected function rows() {return array_values(df_store_names());}
 }
