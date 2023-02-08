dba0628ce771d31b70241de77fbed796c125e579
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-08 07:03:30 +0000

    https://github.com/mage2pro/core/issues/174

--- Config/Next/Backend.php
+++ Config/Next/Backend.php
@@ -70,7 +70,7 @@ class Backend extends Model {
 	 * @return int[][]
 	 */
 	private function nextNumbersFromDb():array {return array_map(function($storeId) {return
-		array_values(array_map(function($entityTypeId) use($storeId) {return
+		array_values(array_map(function(string $entityTypeId) use($storeId) {return
 			df_sales_seq_next($entityTypeId, $storeId)
 		;}, df_sales_entity_types()));
 	}, df_store_ids());}
