710bc261ed4623d77fda62e37547718e00b1887f
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-08 07:07:24 +0000

    https://github.com/mage2pro/core/issues/168

--- Plugin/Model/Manager.php
+++ Plugin/Model/Manager.php
@@ -1,6 +1,7 @@
 <?php
 namespace Dfe\SalesSequence\Plugin\Model;
 use Dfe\SalesSequence\Settings as S;
+use Magento\Framework\DB\Sequence\SequenceInterface as ISequence;
 use Magento\SalesSequence\Model\Manager as Sb;
 use Magento\SalesSequence\Model\Sequence;
 class Manager extends Sb {
@@ -12,9 +13,8 @@ class Manager extends Sb {
 	 * Цель перекрытия — настройка форматирования номеров продажных документов.
 	 * @see \Magento\SalesSequence\Model\Manager::getSequence()
 	 * https://github.com/magento/magento2/blob/720667e/app/code/Magento/SalesSequence/Model/Manager.php#L37-L54
-	 * @return \Magento\Framework\DB\Sequence\SequenceInterface
 	 */
-	function aroundGetSequence(Sb $sb, \Closure $f, string $entityType, int $storeId) {
+	function aroundGetSequence(Sb $sb, \Closure $f, string $entityType, int $storeId):ISequence {
 		$this->type = $entityType;
 		$this->storeId = $storeId;
 		return !S::s()->enable($storeId) ? $f($entityType, $storeId) : df_new_om(Sequence::class, [
